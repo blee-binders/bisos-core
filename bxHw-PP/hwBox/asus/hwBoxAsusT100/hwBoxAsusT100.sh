133b52b1938827856c711dd3044499c77abe0ebc
BISOS Default Usage Acct <bystar@intra-Latitude-7390>
2020-09-22 11:12:22 -0700

    brought over bxHw-PP.

---
+++ bxHw-PP/hwBox/asus/hwBoxAsusT100/hwBoxAsusT100.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+
+IimBriefDescription="NOTYET: Short Description Of The Module"
+
+ORIGIN="
+* Revision And Libre-Halaal CopyLeft -- Part Of ByStar -- Best Used With Blee
+"
+
+####+BEGIN: bx:dblock:bash:top-of-file :vc "cvs" partof: "bystar" :copyleft "halaal+brief"
+typeset RcsId="$Id: hwBoxAsusT100.sh,v 1.1 2020-05-10 01:46:03 lsipusr Exp $"
+# *CopyLeft*
+# Copyright (c) 2011 Neda Communications, Inc. -- http://www.neda.com
+# See PLPC-120001 for restrictions.
+# This is a Halaal Poly-Existential intended to remain perpetually Halaal.
+####+END:
+
+__author__="
+* Authors: Mohsen BANAN, http://mohsen.banan.1.byname.net/contact
+"
+
+
+####+BEGIN: bx:dblock:lsip:bash:seed-spec :types "seedActions.bash"
+SEED="
+*  /[dblock]/ /Seed/ :: [[file:/opt/public/osmt/bin/seedActions.bash]] | 
+"
+FILE="
+*  /This File/ :: /opt/public/osmt/bin/hwBoxAsusT100.sh 
+"
+if [ "${loadFiles}" == "" ] ; then
+    /opt/public/osmt/bin/seedActions.bash -l $0 "$@" 
+    exit $?
+fi
+####+END:
+
+_CommentBegin_
+####+BEGIN: bx:dblock:global:file-insert-cond :cond "./blee.el" :file "/libre/ByStar/InitialTemplates/software/plusOrg/dblock/inserts/topControls.org"
+*      ================
+*  /Controls/ ::  [[elisp:(org-cycle)][| ]]  [[elisp:(show-all)][Show-All]]  [[elisp:(org-shifttab)][Overview]]  [[elisp:(progn (org-shifttab) (org-content))][Content]] | [[file:Panel.org][Panel]] | [[elisp:(blee:ppmm:org-mode-toggle)][Nat]] | [[elisp:(bx:org:run-me)][Run]] | [[elisp:(bx:org:run-me-eml)][RunEml]] | [[elisp:(delete-other-windows)][(1)]] | [[elisp:(progn (save-buffer) (kill-buffer))][S&Q]]  [[elisp:(save-buffer)][Save]]  [[elisp:(kill-buffer)][Quit]] [[elisp:(org-cycle)][| ]]
+** /Version Control/ ::  [[elisp:(call-interactively (quote cvs-update))][cvs-update]]  [[elisp:(vc-update)][vc-update]] | [[elisp:(bx:org:agenda:this-file-otherWin)][Agenda-List]]  [[elisp:(bx:org:todo:this-file-otherWin)][ToDo-List]] 
+
+####+END:
+_CommentEnd_
+
+_CommentBegin_
+*      ================
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]] CONTENTS-LIST ################
+*  [[elisp:(org-cycle)][| ]]  Notes         :: *[Current-Info:]*  Status, Notes (Tasks/Todo Lists, etc.) [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+function vis_moduleDescription {  cat  << _EOF_
+*  [[elisp:(org-cycle)][| ]]  Xrefs         :: *[Related/Xrefs:]*  <<Xref-Here->>  -- External Documents  [[elisp:(org-cycle)][| ]]
+**  [[elisp:(org-cycle)][| ]]  Panel        :: [[file:/libre/ByStar/InitialTemplates/activeDocs/bxServices/versionControl/fullUsagePanel-en.org::Xref-VersionControl][Panel Roadmap Documentation]] [[elisp:(org-cycle)][| ]]
+*  [[elisp:(org-cycle)][| ]]  Info          :: *[Module Description:]* [[elisp:(org-cycle)][| ]]
+**  [[elisp:(org-cycle)][| ]]  Ubuntu From Distro    ::  Old John Wells -- Does Not Work Well   [[elisp:(org-cycle)][| ]]
+***  [[elisp:(org-cycle)][| ]]  Grub Line    ::  dh1,gpt3 /dev/sda3  -- ASUS T100TAM  [[elisp:(org-cycle)][| ]]
+***  [[elisp:(org-cycle)][| ]]  brcmfmac     ::  wifi driver Not Working with kernel 4.4.0-10  [[elisp:(org-cycle)][| ]]
+*** Misc
+**** Use Flash Manage
+**** Skip Boot as it is too small
+
+https://wiki.debian.org/InstallingDebianOn/Asus/T100TA
+
+
+http://www.jfwhome.com/2016/01/04/latest-steps-to-install-ubuntu-on-the-asus-t100ta/
+
+6. Completing the installation
+
+Time to fix the bootloader. To do this, we can just install grub-efi-ia32:
+
+
+sudo apt-get update
+sudo apt-get install grub-efi-ia32
+
+We need to add a line to the kernel boot options to prevent disk corruption.
+Edit the grub configuration file:
+
+sudo nano /etc/default/grub
+
+Find the line starting GRUB_CMDLINE_LINUX_DEFAULT and add intel_idle.max_cstate=0 before quiet splash".
+
+Then ctrl-o, ctrl-x to save & exit, and type:
+sudo update-grub to update Grub.
+
+**  [[elisp:(org-cycle)][| ]]  Ubuntu custom image   ::  2016 Google Plus   [[elisp:(org-cycle)][| ]]
+*** First shrink the windows partition to make room for Linux
+**** http://www.everydaylinuxuser.com/2015/11/how-to-shrink-windows-10-to-make-space.html
+*** From Google Plus https://drive.google.com/drive/folders/0B4s5KNXf2Z36QW9acnY4RXd3bW8
+*** On a ASUSt100Chi get a usb hub and connect keyboard and mouse
+***   [[elisp:(lsip-local-run-command "acroread /uniform/Public/lh-repub/distro/ubuntu/1604/custom/asus-t100/20161006/ASUS-T100TA-INSTALL-INSTRUCTIONS.pdf")][acroread ASUS-T100TA-INSTALL-INSTRUCTIONS.pdf]]
+
+
+*** other.iso booted
+**  [[elisp:(org-cycle)][| ]]  Ubuntu custom image   ::  20170716 Google Plus -- A New ISO Image Is Being Built Soon -- Check agin in august  [[elisp:(org-cycle)][| ]]
+_EOF_
+}
+
+_CommentBegin_
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]]  *Seed Extensions*
+_CommentEnd_
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  Imports       :: Prefaces (Imports/Libraries) [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+. ${opBinBase}/opAcctLib.sh
+. ${opBinBase}/opDoAtAsLib.sh
+. ${opBinBase}/lpParams.libSh
+. ${opBinBase}/lpReRunAs.libSh
+
+
+# PRE parameters
+
+baseDir=""
+
+function G_postParamHook {
+     return 0
+}
+
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  Examples      :: Examples [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+
+function vis_examples {
+    typeset extraInfo="-h -v -n showRun"
+    #typeset extraInfo=""
+    typeset runInfo="-p ri=lsipusr:passive"
+
+    typeset examplesInfo="${extraInfo} ${runInfo}"
+
+    visLibExamplesOutput ${G_myName} 
+  cat  << _EOF_
+$( examplesSeperatorTopLabel "${G_myName}" )
+$( examplesSeperatorChapter "Chapter Title" )
+$( examplesSeperatorSection "Section Title" )
+${G_myName} ${extraInfo} -i doTheWork
+_EOF_
+}
+
+noArgsHook() {
+  vis_examples
+}
+
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  IIFs          :: Interactively Invokable Functions (IIF)s |  [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+
+
+function vis_doTheWork {
+    G_funcEntry
+    function describeF {  G_funcEntryShow; cat  << _EOF_
+_EOF_
+    }
+    EH_assert [[ $# -eq 0 ]]
+
+    lpDo vis_failExample
+    EH_retOnFail
+
+    lpReturn
+}
+
+_CommentBegin_
+*  [[elisp:(beginning-of-buffer)][Top]] ################ [[elisp:(delete-other-windows)][(1)]]  *End Of Editable Text*
+_CommentEnd_
+
+####+BEGIN: bx:dblock:bash:end-of-file :type "basic"
+_CommentBegin_
+*  [[elisp:(org-cycle)][| ]]  Common        ::  /[dblock] -- End-Of-File Controls/ [[elisp:(org-cycle)][| ]]
+_CommentEnd_
+#+STARTUP: showall
+#local variables:
+#major-mode: sh-mode
+#fill-column: 90
+# end:
+####+END:
