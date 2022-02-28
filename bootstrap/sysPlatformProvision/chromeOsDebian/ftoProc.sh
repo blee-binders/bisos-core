55b3e21c00ed0a092509cd372e17ebcdc6e456fe
Mohsen Banan <form@mohsen.banan.1.byname.net>
2022-02-28 10:13:02 -0800

    chromeOsDebian.

--- bootstrap/sysPlatformProvision/chromeOsDebian/ftoProc.sh
+++ bootstrap/sysPlatformProvision/chromeOsDebian/ftoProc.sh
@@ -16,7 +16,7 @@ SEED="
 *  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
 "
 FILE="
-*  /This File/ :: /bisos/git/auth/bxRepos/blee-binders/bisos-core/bootstrap/sysPlatformProvision/ftoProc.sh 
+*  /This File/ :: /bisos/git/auth/bxRepos/blee-binders/bisos-core/bootstrap/sysPlatformProvision/chromeOsDebian/ftoProc.sh 
 "
 if [ "${loadFiles}" == "" ] ; then
     /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
